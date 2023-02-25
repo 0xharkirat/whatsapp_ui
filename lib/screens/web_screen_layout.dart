1e1ad39bf00e718d331b60f3971a9e9e59049280
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2023-02-25 14:58:00 +1100

    display web contact list

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/widgets/contact_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("This is web"),
-      ),
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget> [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: const <Widget> [
+                  ContactList(),
+                ],
+              ),
+            ),
+          ),
+          // Web Screen
+          Container(
+            width: MediaQuery.of(context).size.width * 0.75,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/backgroundImage.png'),
+                fit: BoxFit.cover,
+              )
+            ),
+          )
+        ],
+      )
     );
   }
 }
--- lib/widgets/contact_list.dart
+++ lib/widgets/contact_list.dart
@@ -9,51 +9,50 @@ class ContactList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 10),
-      child: Column(
-        children: <Widget> [
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: info.length,
-              itemBuilder: (context, index){
-                return InkWell(
-                  onTap: (){},
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: ListTile(
-                      title: Text(
-                        info[index]['name'].toString(),
-                        style: const TextStyle(
-                          fontSize: 18.0,
-                        ),
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: info.length,
+        itemBuilder: (context, index){
+          return Column(
+            children: <Widget> [
+              InkWell(
+                onTap: (){},
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: ListTile(
+                    title: Text(
+                      info[index]['name'].toString(),
+                      style: const TextStyle(
+                        fontSize: 18.0,
                       ),
-                      subtitle: Padding(
-                        padding: const EdgeInsets.only(top: 6.0),
-                        child: Text(
-                          info[index]['message'].toString(),
-                          style: const TextStyle(
-                            fontSize: 15.0
-                            ),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(top: 6.0),
+                      child: Text(
+                        info[index]['message'].toString(),
+                        style: const TextStyle(
+                          fontSize: 15.0
                           ),
                         ),
-                      leading: CircleAvatar(
-                        backgroundImage: NetworkImage(info[index]['profilePic'].toString()),
-                        ),
-                      trailing: Text(
-                        info[index]['time'].toString(),
-                        style: const TextStyle(
-                          fontSize: 13.0,
-                          color: Colors.grey,
-                        ),
+                      ),
+                    leading: CircleAvatar(
+                      radius: 30.0,
+                      backgroundImage: NetworkImage(info[index]['profilePic'].toString()),
+                      ),
+                    trailing: Text(
+                      info[index]['time'].toString(),
+                      style: const TextStyle(
+                        fontSize: 13.0,
+                        color: Colors.grey,
                       ),
                     ),
                   ),
-                );
-              },
-            ),
-          ),
-          const Divider(color: dividerColor, indent: 85,)
-        ],
+                ),
+              ),
+              const Divider(color: dividerColor,)
+            ],
+          );
+        },
       ),
     );
   }
