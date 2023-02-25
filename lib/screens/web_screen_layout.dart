32864c58a4b614a65c58966506c3e10fe6563d13
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2023-02-25 16:51:11 +1100

    display web chat input

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/colors.dart';
 import 'package:whatsapp_ui/widgets/chat_list.dart';
 import 'package:whatsapp_ui/widgets/web_chat_app_bar.dart';
 import 'package:whatsapp_ui/widgets/web_search_bar.dart';
@@ -35,9 +36,48 @@ class WebScreenLayout extends StatelessWidget {
               )
             ),
             child: Column(
-              children: const <Widget> [
-                WebChatAppBar(),
-                Expanded(child: ChatList()),
+              children:  <Widget> [
+                const WebChatAppBar(),
+                const Expanded(child: ChatList()),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: dividerColor,
+                      ),
+                    ),
+                    color: chatBarMessage,
+                  ),
+                  child: Row(
+                    children: <Widget> [
+                      IconButton(onPressed: (){}, icon: const Icon(Icons.emoji_emotions_outlined, color: Colors.grey,),),
+                      IconButton(onPressed: (){}, icon: const Icon(Icons.attach_file, color: Colors.grey,),),
+                      Expanded(child: Padding(
+                        padding: const EdgeInsets.only(left: 10, right: 15),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            fillColor: searchBarColor,
+                            filled: true,
+                            hintText: 'Type a Message',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                              borderSide: const BorderSide(
+                                width: 0,
+                                style: BorderStyle.none,
+                              ),
+                            ),
+                            contentPadding: EdgeInsets.only(left: 20),
+                          ),
+                        ),
+                      ),),
+                      IconButton(onPressed: (){}, icon: const Icon(Icons.mic, color: Colors.grey,),),
+
+                    ],
+                  ),
+
+                )
               ],
             ),
           )
